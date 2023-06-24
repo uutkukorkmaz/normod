dbcc2f2d1be0da687bc627f4e70f8e11a8178ffd
Utku Korkmaz <utku.korkmaz@karaca.com>
2023-06-24 23:00:19 +0300

    initial commit

---
+++ src/app/Http/Controllers/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Normod Coding Case Documentation",
+ *     description="An example study of a order service",
+ *     @OA\Contact(
+ *     email="uutkukorkmaz@gmail.com",
+ *     name="Utku Korkmaz"
+ *    )
+ * )
+ */
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
