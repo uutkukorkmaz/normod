196659a38a3a4ddf9e5cc9132eff4a8191c93fd4
Utku Korkmaz <utku.korkmaz@karaca.com>
2023-06-29 20:12:46 +0300

    make sure every customer has at least one address

--- src/database/seeders/AddressSeeder.php
+++ src/database/seeders/AddressSeeder.php
@@ -14,8 +14,8 @@ class AddressSeeder extends Seeder
      */
     public function run(): void
     {
-        Customer::inRandomOrder()->take(500)->get()->each(function ($customer) {
-            $addresses = Address::factory(rand(0, 3))->create(['customer_id' => $customer->id]);
+        Customer::inRandomOrder()->get()->each(function ($customer) {
+            $addresses = Address::factory(rand(1, 3))->create(['customer_id' => $customer->id]);
             $addresses->load('customer')
                 ->each(function ($address) {
                     echo "\t Created address: " . $address->name . ' for customer: ' . $address->customer->name . PHP_EOL;
