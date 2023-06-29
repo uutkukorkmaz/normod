4f7c934ec895409d9ca521002f927a2bceb2c75c
Utku Korkmaz <utku.korkmaz@karaca.com>
2023-06-29 12:42:43 +0300

    add health route for development purposes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/health', function () {
+
+    return [
+        'db' => DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME),
+        'redis' => \Illuminate\Support\Facades\Redis::connection()->ping(),
+    ];
 });
